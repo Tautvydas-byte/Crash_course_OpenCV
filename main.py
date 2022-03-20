a51c78fcd9cfa0c97dd84cd4d3e9ec71b72f5502
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2022-03-20 23:27:04 +0200

    "added colors,buttons,scores"

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ button.add_button("cell phone", 20, 80)
 button.add_button("scissors", 20, 140)
 button.add_button("clock", 20, 200)
 
+colors = button.colors
 
 # Opencv DNN(deep neural network)
 net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")  # yolov4 loaded
@@ -29,12 +30,10 @@ camera = cv2.VideoCapture(camera_port, cv2.CAP_DSHOW)  # cap
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # pacios naudojamos kameros rezoliucija
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-
 # ----Create window-----------
 def click_button(event, x, y, flags, params):
 	if event == cv2.EVENT_LBUTTONDOWN:
 		button.button_click(x, y)
-
 
 cv2.namedWindow("Frame")
 cv2.setMouseCallback("Frame", click_button)
@@ -57,14 +56,15 @@ while camera.isOpened():
 		(x, y, w, h) = bounding_box  # coordinates
 		# print(x,y,w,h)
 		class_name = classes[class_id]
+		color = colors[class_id]
 
 		if class_name in active_buttons:
-			cv2.putText(frame, str(class_name), (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, (200, 0, 50), 2)
-			cv2.rectangle(frame, (x, y), (x + w, y + w), (200, 0, 50), 3)
+			cv2.putText(frame, str(class_name), (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
+			cv2.putText(frame, str(score), (x+100, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
+			cv2.rectangle(frame, (x, y), (x + w, y + w), color, 3)
 
 	# Display buttons
 	button.display_buttons(frame)
-
 	# ---------------------------
 	cv2.imshow("Frame", frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
